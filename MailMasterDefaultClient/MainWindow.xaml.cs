1b80ad88bc9699eea7dc88657578dafd62a7ac02
L!εsAμεr <liesauer@liesauer.net>
2021-12-31 20:22:54 +0800

    网易邮箱大师默认客户端

---
+++ MailMasterDefaultClient/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.Win32;
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+using AppResources = MailMasterDefaultClient.Properties.Resources;
+
+namespace MailMasterDefaultClient
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            InitializeComponent();
+
+            this.MinWidth = this.MaxWidth = this.Width;
+            this.MinHeight = this.MaxHeight = this.Height;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var result = MessageBox.Show("是否将网易邮箱大师设置为默认客户端？", "设置默认客户端", MessageBoxButton.YesNo, MessageBoxImage.None, MessageBoxResult.No);
+
+            if (result != MessageBoxResult.Yes) return;
+
+            //为null表示没有安装
+            //为“”表示安装不正确
+            string progid = null;
+
+            try
+            {
+                var mailKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Clients\\Mail");
+
+                if (mailKey != null)
+                {
+                    var keyNames = mailKey.GetSubKeyNames();
+
+                    var masterKeyName = "";
+
+                    foreach (var name in keyNames)
+                    {
+                        if (!name.Contains("网易邮箱大师")) return;
+
+                        masterKeyName = name;
+                        break;
+                    }
+
+                    if (masterKeyName != "")
+                    {
+                        progid = "";
+
+                        var URLAssociations = mailKey.OpenSubKey(masterKeyName + "\\Capabilities\\URLAssociations");
+
+                        var mailto = URLAssociations.GetValue("mailto");
+
+                        if (mailto != null)
+                        {
+                            progid = mailto.ToString();
+                        }
+                    }
+                }
+
+                if (progid == null)
+                {
+                    MessageBox.Show("没有安装网易邮箱大师！");
+                }
+                else if (progid == "")
+                {
+                    MessageBox.Show("网易邮箱大师安装不正确，请尝试重装！");
+                }
+                else
+                {
+                    //释放SetUserFTA
+
+                    var AppDataDir = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+                    var AppDir = AppDataDir + "\\MailMasterDefaultClient";
+
+                    var SetUserFTAPath = AppDir + "\\SetUserFTA.exe";
+
+                    if (!Directory.Exists(AppDir))
+                    {
+                        Directory.CreateDirectory(AppDir);
+                    }
+
+                    File.WriteAllBytes(SetUserFTAPath, AppResources.SetUserFTA);
+
+                    //运行SetUserFTA
+
+                    var psi = new ProcessStartInfo(SetUserFTAPath)
+                    {
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        RedirectStandardOutput = false,
+                        RedirectStandardError = false,
+                    };
+
+                    psi.Arguments = "mailto " + progid;
+
+                    var process = Process.Start(psi);
+
+                    process.WaitForExit();
+                    
+                    var needTest = MessageBox.Show("设置默认客户端成功！是否需要测试一下？\n\n注意事项：\n如果弹出需要选择默认程序的对话框，选一下Win10自带的邮件客户端（或者其他任意的邮件客户端也行），然后再点一次按钮再次设置网易邮箱大师为默认客户端，否则不会成功！！！", "设置默认客户端", MessageBoxButton.YesNo, MessageBoxImage.None, MessageBoxResult.No);
+
+                    if (needTest == MessageBoxResult.Yes)
+                    {
+                        Process.Start(new ProcessStartInfo("explorer.exe")
+                        {
+                            Arguments = "mailto:liesauer@hotmail.com",
+                            UseShellExecute = false,
+                            CreateNoWindow = true,
+                            RedirectStandardOutput = false,
+                            RedirectStandardError = false,
+                        });
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("设置默认客户端失败！");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="center">网易邮箱大师默认客户端</h1>
+<h3 align="center">
+    <p>一键设置网易邮箱大师为默认邮件客户端</p>
+    <img src="screenshot.png">
+</h3>
+
+## 为什么要特意做这么一个工具？
+
+这是因为网易邮箱大师在Win10中是没办法通过正常方法设置为默认客户端的，看下图。
+
+![screenshot_2.png](screenshot_2.png)
